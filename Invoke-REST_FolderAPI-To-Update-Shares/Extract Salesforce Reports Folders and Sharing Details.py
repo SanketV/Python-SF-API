93c5c64d8b7910ad8475b57d81d3702a21fe54c0
Sanket V <sanket.vaidya@jackson.com>
2021-03-22 12:11:54 -0500

    Updated Extract SF report folders file.

--- Invoke-REST_FolderAPI-To-Update-Shares/Extract Salesforce Reports Folders and Sharing Details.py
+++ Invoke-REST_FolderAPI-To-Update-Shares/Extract Salesforce Reports Folders and Sharing Details.py
@@ -1,18 +1,6 @@
 import csv
 import requests
-params = {
-    "grant_type": "password",
-    "client_id": "3MVG9FS3IyroMOh6kcLgYOf5qUWRieofUkdkebMP7qwwqaNC8bP_9HIeWsyPfKYjS3vmKZpJQJLtuaFbvKHGp", # Consumer Key
-    "client_secret": "BBFA7D464EF0BBBB713EA1007BBF03B207DE89495AE2058B4A6E2513ADB62ECA", # Consumer Secret
-    "username": "sanket.vaidya@texascapitalbank.com.uat1", # The email you use to login
-    "password": "Plano1234." # Concat your password and your security token
-}
-r = requests.post("https://test.salesforce.com/services/oauth2/token", params=params)
-access_token = r.json().get("access_token")
-instance_url = r.json().get("instance_url")
-print("Access Token:", access_token)
-print("Instance URL", instance_url)
-print("response : " , r)
+from SF_API import SalesforceAPIBangBang
 
 class Folder:
     id = ''
@@ -30,37 +18,9 @@ class Shares:
     sharedWithId = ''
     sharedWithLabel = ''
     
-    
 
-def sf_api_call(action, parameters = {}, method = 'get', data = {}):
-    """
-    Helper function to make calls to Salesforce REST API.
-    Parameters: action (the URL), URL params, method (get, post or patch), data for POST/PATCH.
-    """
-    headers = {
-        'Content-type': 'application/json',
-        'Accept-Encoding': 'gzip',
-        'Authorization': 'Bearer %s' % access_token
-    }
-    if method == 'get':
-        r = requests.request(method, instance_url+action, headers=headers, params=parameters, timeout=30)
-    elif method in ['post', 'patch']:
-        r = requests.request(method, instance_url+action, headers=headers, json=data, params=parameters, timeout=10)
-    else:
-        # other methods not implemented in this example
-        raise ValueError('Method should be get or post or patch.')
-    print('Debug: API %s call: %s - StatusCode %s' % (method, r.url, r.status_code) )    
-    
-    if r.status_code < 300:
-        if method=='patch':
-            return None
-        else:
-            return r.json()
-    else:
-        raise Exception('API error when calling %s : %s' % (r.url, r.content))
-        
-
-foldersJSON = sf_api_call('/services/data/v46.0/folders?page=1&pageSize=300')
+salesforceAPI = SalesforceAPIBangBang()
+foldersJSON = salesforceAPI.sf_api_call(action='/services/data/v46.0/folders?page=1&pageSize=300')
 
 folders = foldersJSON["folders"]
 
@@ -79,10 +39,10 @@ for f in folders:
     fld.type = f['type']
     fld.url = f['url']
     
-    folderSharesUrl = sf_api_call(f['url'])['sharesUrl']
+    folderSharesUrl = salesforceAPI.sf_api_call(f['url'])['sharesUrl']
     fld.folderSharesUrl = folderSharesUrl
     
-    folderShares = sf_api_call(folderSharesUrl)["shares"]
+    folderShares = salesforceAPI.sf_api_call(folderSharesUrl)["shares"]
 
     fldSharesList = []    
         
@@ -98,7 +58,7 @@ for f in folders:
     fld.shares = fldSharesList
     foldersList.append(fld)
 
-        #print("• Share : " + fldShare.accessType + ' , ' + fldShare.shareType + ' - ' + fldShare.sharedWithLabel)
+    #print("• Share : " + fldShare.accessType + ' , ' + fldShare.shareType + ' - ' + fldShare.sharedWithLabel)
     
    
 def make_csv(foldersList):
